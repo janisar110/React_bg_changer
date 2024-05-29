7e6dedbf65ef45add69222a37fb202d418939285
janisar110 <janisarhasnain@gmail.com>
2024-05-29 14:31:31 +0500

    bg-changer

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+
+function App() {
+
+  const [color,setColor] = useState("aqua")
+
+  return (
+    <>
+    <div className="w-full h-screen"
+      style={{backgroundColor:color}}
+     >
+    <div className="grid h-screen">
+      <div className="bg-slate-700 w-max px-10 grid col-span-1">
+        <button className="cursor-pointer px-8 py-3 rounded-2xl text-white text-2xl my-6"
+        style={{backgroundColor: "red"}}
+        onClick={()=>setColor("red")}
+        >Red</button>
+         <button className="cursor-pointer px-8 py-3 rounded-2xl text-white text-2xl my-6"
+        style={{backgroundColor: "blue"}}
+        onClick={()=>setColor("blue")}
+        >blue</button>
+         <button className="cursor-pointer px-8 py-3 rounded-2xl text-white text-2xl my-6"
+        style={{backgroundColor: "green"}}
+        onClick={()=>setColor("green")}
+        >green</button>
+         <button className="cursor-pointer px-8 py-3 rounded-2xl text-black text-2xl my-6"
+        style={{backgroundColor: "yellow"}}
+        onClick={()=>setColor("yellow")}
+        >yellow</button>
+         <button className="cursor-pointer px-8 py-3 rounded-2xl text-black text-2xl my-6"
+        style={{backgroundColor: "white"}}
+        onClick={()=>setColor("white")}
+        >white</button>
+         <button className="cursor-pointer px-8 py-3 rounded-2xl text-white text-2xl my-6"
+        style={{backgroundColor: "black"}}
+        onClick={()=>setColor("black")}
+        >black</button>
+         <button className="cursor-pointer px-8 py-3 rounded-2xl text-white text-2xl my-6"
+        style={{backgroundColor: "orange"}}
+        onClick={()=>setColor("orange")}
+        >black</button>
+      </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
